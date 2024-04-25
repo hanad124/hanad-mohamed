d9d2c2f9c51fcc493c5359bcd3dc6bf1620095fe
Hanad Mohamed <hanaddaahir124@gmail.com>
2024-04-25 23:11:54 +0300

    Added SEO.

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,22 @@ import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "@/components/theme-provide";
-import {Helmet} from "react-helmet";
+import { Helmet } from "react-helmet";
 
+import thumb from "../public/portfolio-thumb.svg";
+
+<Helmet>
+  <meta property="og:title" content={"Hanad Mohamed - MERN stack Developer"} />
+  <meta
+    property="og:description"
+    content={
+      "I am a MERN stack developer with a passion for building web applications. I have experience in building full-stack web applications using React, Node.js, Express, and MongoDB. I am currently looking for new opportunities to work on exciting projects"
+    }
+  />
+  <meta property="og:image" content={thumb} />
+  <meta property="og:url" content={"https://hanad-mohamed.vercel.app/"} />
+  <meta property="og:type" content="website" />
+</Helmet>;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
